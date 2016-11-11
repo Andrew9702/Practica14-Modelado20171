500cf9a17ee4626761864c783bf4d14f5a55ed77
AndresBarbosa <andresbob@ciencias.unam.mx>
2016-11-10 21:15:43 -0600

    Primer backup

---
+++ main.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/*Función que inserta números aleatorios en una lista*/
+void inserta_datos_de_prueba(Lista lista);
+
+int main()
+{
+    // Se crea la lista
+    Lista lista = crea_lista();
+    printf("La lista tiene %d elementos\n", longitud(lista));
+
+    // Se insertan datos de prueba
+    inserta_datos_de_prueba(lista);
+    printf("La lista tiene %d elementos\n", longitud(lista));
+
+    // Se remueve un elemento de la lista
+    Elemento *borrado = quita_elemento(lista, 0);
+    if (borrado != NULL) {free(borrado->valor);}
+    free(borrado);
+    printf("La lista tiene %d elementos\n", longitud(lista));
+
+    // Se remueve un elemento que no existe en la lista
+    quita_elemento(lista, longitud(lista));
+    printf("La lista tiene %d elementos\n", longitud(lista));
+
+    //Se imprime la lista antes de ordenar
+    imprime_lista_int(lista);
+    ordena_lista(lista, &cmp_int);
+
+    //Se imprime la lista después de ordenar
+    imprime_lista_int(lista);
+
+    //Se libera la memoria ocupada
+    borra_lista(lista);
+}
+
+void inserta_datos_de_prueba(Lista lista)
+{
+    srand(time(NULL));
+    int *num_a_insertar;
+    int indice;
+    for (indice = 0; indice < 20; ++indice) {
+        num_a_insertar = malloc(sizeof(int));
+        *num_a_insertar = rand() % 100;
+        inserta_elemento(lista, num_a_insertar);
+    };
+}
